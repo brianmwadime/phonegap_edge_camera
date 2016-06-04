fcae4a7cb92c1a1c78049f1020ee66c8328e9064
Brian Mwadime <mwadime@fortunekidew.co.ke>
2016-06-04 16:55:19 +0300

    Update

--- platforms/ios/EdgeCamera/Classes/EdgeCamera.m
+++ platforms/ios/EdgeCamera/Classes/EdgeCamera.m
@@ -16,31 +16,31 @@
 
 // Cordova command method
 -(void) takePicture:(CDVInvokedUrlCommand *)command {
-    
+
     // Set the hasPendingOperation field to prevent the webview from crashing
     self.hasPendingOperation = YES;
-    
+
     // Save the CDVInvokedUrlCommand as a property.  We will need it later.
     self.latestCommand = command;
-    
+
     // Make the overlay view controller.
     self.overlay = [[ViewController alloc] init];
 //    [self.overlay set ];
     self.overlay.plugin = self;
-    
+
     // Display the view.  This will "slide up" a modal view from the bottom of the screen.
     [self.viewController presentViewController:self.overlay animated:YES completion:nil];
 }
 
-// Method called by the overlay when the image is ready to be sent back to the web view
--(void) capturedImageWithPath:(NSString*)imagePath {
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imagePath] callbackId:self.latestCommand.callbackId];
-    
-    // Unset the self.hasPendingOperation property
-    self.hasPendingOperation = NO;
-    
-    // Hide the picker view
-    [self.viewController dismissModalViewControllerAnimated:YES];
-}
+// // Method called by the overlay when the image is ready to be sent back to the web view
+// -(void) capturedImageWithPath:(NSString*)imagePath {
+//     [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imagePath] callbackId:self.latestCommand.callbackId];
+
+//     // Unset the self.hasPendingOperation property
+//     self.hasPendingOperation = NO;
+
+//     // Hide the picker view
+//     [self.viewController dismissModalViewControllerAnimated:YES];
+// }
 
 @end
